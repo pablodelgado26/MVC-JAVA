a7b50e06c08bc9f4fc843071cc95ab79fad0b140
Pablo Delgado <pablo.j.abreu@aluno.senai.br>
2025-05-05 18:48:07 -0300

    feat: Primeiros passos com o Thymeleaf

--- src/main/java/br/com/xavecoding/regescweb/controllers/helloController.java
+++ src/main/java/br/com/xavecoding/regescweb/controllers/helloController.java
@@ -2,15 +2,30 @@ package br.com.xavecoding.regescweb.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.Model;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class helloController {
     
     @GetMapping("/hello")
-    public String hello(){
-        return "hello";
+    public ModelAndView hello() {
+        ModelAndView mv = new ModelAndView("hello"); // Retorna o nome da view "hello.html"
+        mv.addObject ("nome", "Augusto dos Anjos"); 
+        return mv; 
+    }
+
+    @GetMapping("/hello-model")
+    public String hello(Model model) {
+        model.addAttribute("nome", "Euclides da Cunha"); 
+        return "hello"; // Retorna o nome da view "hello.html"
+    }
+
+    @GetMapping("/hello-servlet")
+    public String hello(HttpServletRequest request) {
+        request.setAttribute("nome", "Vargas"); 
+        return "hello"; // Retorna o nome da view "hello.html"
     }
     
 }
